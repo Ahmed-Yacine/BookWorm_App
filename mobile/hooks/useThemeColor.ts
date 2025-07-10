e802172a5a41e9a99b1c40b480764a683d73e957
Ahmed-Yacine <ay.boutera@esi-sba.dz>
2025-07-10 18:23:30 +0100

    init

---
+++ mobile/hooks/useThemeColor.ts
@@ -0,0 +1,22 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+  const theme = useColorScheme() ?? "light";
+  const colorFromProps = props[theme];
+
+  // If the color is provided in props, use that; otherwise, use the default from Colors
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return Colors[theme][colorName];
+  }
+}
