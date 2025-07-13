f693c312c4650a4dae1073c4f7487d32a05aa270
Ahmed-Yacine <ay.boutera@esi-sba.dz>
2025-07-13 18:41:24 +0100

    comment functionality

--- backend/src/comment/comment.controller.ts
+++ backend/src/comment/comment.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+  Param,
+  ParseIntPipe,
+  Get,
+  Delete,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { AuthGuard } from '../user/guards/auth.guard';
+import { CreateCommentDto } from './dtos/create-comment.dto';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @Get('/post/:postId')
+  public getComments(@Param('postId', ParseIntPipe) postId: number) {
+    return this.commentService.getComments(postId);
+  }
+
+  @Post('/post/:postId')
+  @UseGuards(AuthGuard)
+  public createComment(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() createCommentDto: CreateCommentDto,
+    @Request() req: any,
+  ) {
+    return this.commentService.createComment(
+      postId,
+      createCommentDto,
+      req.user.id,
+    );
+  }
+
+  @Delete(':commentId')
+  @UseGuards(AuthGuard)
+  public deleteComment(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Request() req: any,
+  ) {
+    return this.commentService.deleteComment(commentId, req.user.id);
+  }
+
+  @Post(':commentId/toggleLike')
+  @UseGuards(AuthGuard)
+  public toggleLikeComment(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Request() req: any,
+  ) {
+    return this.commentService.toggleLikeComment(commentId, req.user.id);
+  }
 }
